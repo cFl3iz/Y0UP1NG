9668c6bc50729fbc690f875d45cb4054e5cff6ee
s784838898 <784838898@qq.com>
2017-11-09 17:33:25 +0800

    Fixed Message Error

--- serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerServices.java
+++ serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerServices.java
@@ -194,7 +194,7 @@ public class PersonManagerServices {
 
 
             try{
-                dispatcher.runSync("pushNotifOrMessage",UtilMisc.toMap("userLogin",admin,"message","message:"+partyIdFrom,"content",partyIdFrom,"regId",jpushId,"deviceType",partyIdentificationTypeId,"sendType",""));
+                dispatcher.runSync("pushNotifOrMessage",UtilMisc.toMap("userLogin",admin,"message","message:"+partyIdFrom,"content",text,"regId",jpushId,"deviceType",partyIdentificationTypeId,"sendType",""));
             } catch (GenericServiceException e1) {
                 Debug.logError(e1.getMessage(), module);
                 return ServiceUtil.returnError(UtilProperties.getMessage(resourceError, "JPushError", locale));
@@ -1213,7 +1213,7 @@ public class PersonManagerServices {
 
 
             try{
-                dispatcher.runSync("pushNotifOrMessage",UtilMisc.toMap("userLogin",admin,"message","order","content",maiJiaName+"购买了您的产品!点我查看!","regId",jpushId,"deviceType",partyIdentificationTypeId,"sendType",""));
+                dispatcher.runSync("pushNotifOrMessage",UtilMisc.toMap("userLogin",admin,"message","order:"+orderId,"content",maiJiaName+"购买了您的产品!点我查看!","regId",jpushId,"deviceType",partyIdentificationTypeId,"sendType",""));
             } catch (GenericServiceException e1) {
                 Debug.logError(e1.getMessage(), module);
                 return ServiceUtil.returnError(UtilProperties.getMessage(resourceError, "JPushError", locale));

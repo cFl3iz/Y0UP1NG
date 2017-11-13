99bb1a315d03ffadc9145010431841c2048e9876
s784838898 <784838898@qq.com>
2017-11-13 15:22:47 +0800

    更改排序

--- serviceSales/hot-deploy/platformmanager/src/main/java/com/banfftech/platformmanager/PlatformManagerQueryServices.java
+++ serviceSales/hot-deploy/platformmanager/src/main/java/com/banfftech/platformmanager/PlatformManagerQueryServices.java
@@ -341,7 +341,7 @@ public class PlatformManagerQueryServices {
         }else{
             queryMessageLogList = delegator.findList("MessageLogView",
                     listBigConditions, fieldSet,
-                    UtilMisc.toList("-fromDate"), null, false);
+                    UtilMisc.toList("fromDate"), null, false);
         }
 
 

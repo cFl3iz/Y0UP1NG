ca07f77a026ee6f3cdba419fde6a4448819d3d7c
s784838898 <784838898@qq.com>
2017-11-14 10:59:23 +0800

    Added Method

--- serviceSales/hot-deploy/platformmanager/src/main/java/com/banfftech/platformmanager/wechat/WeChatUtil.java
+++ serviceSales/hot-deploy/platformmanager/src/main/java/com/banfftech/platformmanager/wechat/WeChatUtil.java
@@ -83,6 +83,50 @@ public class WeChatUtil {
 
 
 
+    public static String sendGet(String url) {
+        String result = "";
+        BufferedReader in = null;
+        try {
+            URL realUrl = new URL(url);
+            // 打开和URL之间的连接������
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用请求属性��������
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立连接���
+            connection.connect();
+            // 获取响应头字段�
+            // Map<String, List<String>> map = connection.getHeaderFields();
+            //  遍历所有的响应头字段ֶ�
+            // for (String key : map.keySet()) {
+            // System.out.println(key + "--->" + map.get(key));
+            // }
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送 get 异常" + e);
+            e.printStackTrace();
+        }
+        // ʹ��finally�����ر�������
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        System.out.println();
+        return result;
+    }
+
+
 
     public static AccessToken getAccessToken(String appid, String appsecret) {
         AccessToken accessToken = null;

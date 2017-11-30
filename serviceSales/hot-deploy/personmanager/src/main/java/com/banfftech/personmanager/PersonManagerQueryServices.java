18a9f367cd82dfd4802caf868b3debee1552b3b6
s784838898 <784838898@qq.com>
2017-11-30 12:41:00 +0800

    Fixed Bug

--- serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerQueryServices.java
+++ serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerQueryServices.java
@@ -602,7 +602,7 @@ public class PersonManagerQueryServices {
                 .makeCondition(UtilMisc.toMap("payToPartyId",partyId));
 
         EntityCondition listConditions = EntityCondition
-                .makeCondition(findConditions,EntityOperator.OR,findConditions2);
+                .makeCondition(findConditions);
 
         EntityCondition listConditions2 = EntityCondition
                 .makeCondition(findConditions3,EntityOperator.AND,listConditions);

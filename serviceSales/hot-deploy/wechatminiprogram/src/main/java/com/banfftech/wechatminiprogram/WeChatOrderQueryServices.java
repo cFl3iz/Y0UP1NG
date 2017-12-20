1da2a94bf9b0750280ebc25f31bf658b36ed3294
s784838898 <784838898@qq.com>
2017-12-20 11:31:55 +0800

    格式化数据

--- serviceSales/hot-deploy/wechatminiprogram/src/main/java/com/banfftech/wechatminiprogram/WeChatOrderQueryServices.java
+++ serviceSales/hot-deploy/wechatminiprogram/src/main/java/com/banfftech/wechatminiprogram/WeChatOrderQueryServices.java
@@ -34,12 +34,12 @@ import java.util.*;
 public class WeChatOrderQueryServices {
 
 
-
     public final static String module = WeChatOrderQueryServices.class.getName();
 
 
     /**
      * query MyCollect Product
+     *
      * @param dctx
      * @param context
      * @return
@@ -56,8 +56,6 @@ public class WeChatOrderQueryServices {
         List<Map<String, Object>> returnList = new ArrayList<Map<String, Object>>();
 
 
-
-
         String openId = (String) context.get("openId");
 
         System.out.println("*OPENID = " + openId);
@@ -66,25 +64,48 @@ public class WeChatOrderQueryServices {
 
         String partyId = "NA";
 
-        if(UtilValidate.isNotEmpty(partyIdentification) ){
+        if (UtilValidate.isNotEmpty(partyIdentification)) {
             partyId = (String) partyIdentification.get("partyId");
         }
-        System.out.println("*partyId = "+partyId);
+        System.out.println("*partyId = " + partyId);
 
-        List<GenericValue> partyMarkRoleList = EntityQuery.use(delegator).from("ProductRole").where("partyId", partyId,"roleTypeId", "PLACING_CUSTOMER").queryList();
 
-        resultMap.put("collectList",partyMarkRoleList);
+        List<GenericValue> partyMarkRoleList = EntityQuery.use(delegator).from("ProductRole").where("partyId", partyId, "roleTypeId", "PLACING_CUSTOMER").queryList();
+
+        if (null != partyMarkRoleList) {
+
+
+            for (GenericValue gv : partyMarkRoleList) {
+
+                String productId  = (String) gv.get("productId");
+
+                Map<String,Object> rowMap =  new HashMap<String, Object>();
+
+                GenericValue product = delegator.findOne("Product",UtilMisc.toMap("productId",productId),false);
+
+                rowMap.put("productId",productId);
+                rowMap.put("internalName",product.get("internalName"));
+                rowMap.put("productName",product.get("productName"));
+                rowMap.put("detailImageUrl",product.get("detailImageUrl"));
+
+                GenericValue productPrice =EntityQuery.use(delegator).from("ProductPrice").where("productId",productId).queryFirst();
+
+                rowMap.put("price",productPrice.get("price"));
+
+                returnList.add(rowMap);
+            }
+
+        }
+
+        resultMap.put("collectList", returnList);
 
         return resultMap;
     }
 
 
-
-
-
-
     /**
      * query Order FromWeChat
+     *
      * @param dctx
      * @param context
      * @return
@@ -101,8 +122,6 @@ public class WeChatOrderQueryServices {
         List<Map<String, Object>> returnList = new ArrayList<Map<String, Object>>();
 
 
-
-
         String openId = (String) context.get("openId");
 
         System.out.println("*OPENID = " + openId);
@@ -111,10 +130,10 @@ public class WeChatOrderQueryServices {
 
         String partyId = "NA";
 
-        if(UtilValidate.isNotEmpty(partyIdentification) ){
+        if (UtilValidate.isNotEmpty(partyIdentification)) {
             partyId = (String) partyIdentification.get("partyId");
         }
-        System.out.println("*partyId = "+partyId);
+        System.out.println("*partyId = " + partyId);
         Set<String> fieldSet = new HashSet<String>();
         fieldSet.add("orderId");
         fieldSet.add("partyId");
@@ -130,7 +149,6 @@ public class WeChatOrderQueryServices {
         fieldSet.add("payToPartyId");
 
 
-
         EntityCondition findConditions3 = EntityCondition
                 .makeCondition(UtilMisc.toMap("roleTypeId", "BILL_TO_CUSTOMER"));
 
@@ -139,61 +157,60 @@ public class WeChatOrderQueryServices {
 
 
         EntityCondition findConditions2 = EntityCondition
-                .makeCondition(UtilMisc.toMap("payToPartyId",partyId));
+                .makeCondition(UtilMisc.toMap("payToPartyId", partyId));
 
         EntityCondition listConditions = EntityCondition
-                .makeCondition(findConditions,EntityOperator.OR,findConditions2);
+                .makeCondition(findConditions, EntityOperator.OR, findConditions2);
 
         EntityCondition listConditions2 = EntityCondition
-                .makeCondition(findConditions3,EntityOperator.AND,listConditions);
+                .makeCondition(findConditions3, EntityOperator.AND, listConditions);
 
 
         List<GenericValue> queryMyResourceOrderList = delegator.findList("OrderHeaderItemAndRoles",
                 listConditions2, fieldSet,
                 UtilMisc.toList("-orderDate"), null, false);
-        System.out.println("*queryMyResourceOrderList = "+queryMyResourceOrderList);
-        if(null != queryMyResourceOrderList && queryMyResourceOrderList.size()>0){
+        System.out.println("*queryMyResourceOrderList = " + queryMyResourceOrderList);
+        if (null != queryMyResourceOrderList && queryMyResourceOrderList.size() > 0) {
 
-            for(GenericValue gv : queryMyResourceOrderList){
-                Map<String,Object> rowMap = new HashMap<String, Object>();
+            for (GenericValue gv : queryMyResourceOrderList) {
+                Map<String, Object> rowMap = new HashMap<String, Object>();
                 rowMap = gv.getAllFields();
                 String productStoreId = (String) gv.get("productStoreId");
                 String productId = (String) gv.get("productId");
-                GenericValue product = delegator.findOne("Product",UtilMisc.toMap("productId",productId),false);
-                GenericValue productStore = delegator.findOne("ProductStore",UtilMisc.toMap("productStoreId",productStoreId),false);
-                rowMap.put("productName",""+product.get("productName"));
-                rowMap.put("detailImageUrl",(String)product.get("detailImageUrl"));
-                String payToPartyId = (String)productStore.get("payToPartyId");
-                rowMap.put("payToPartyId",payToPartyId);
+                GenericValue product = delegator.findOne("Product", UtilMisc.toMap("productId", productId), false);
+                GenericValue productStore = delegator.findOne("ProductStore", UtilMisc.toMap("productStoreId", productStoreId), false);
+                rowMap.put("productName", "" + product.get("productName"));
+                rowMap.put("detailImageUrl", (String) product.get("detailImageUrl"));
+                String payToPartyId = (String) productStore.get("payToPartyId");
+                rowMap.put("payToPartyId", payToPartyId);
                 String statusId = (String) gv.get("statusId");
-                rowMap.put("statusId",UtilProperties.getMessage("PersonManagerUiLabels.xml", statusId, locale));
+                rowMap.put("statusId", UtilProperties.getMessage("PersonManagerUiLabels.xml", statusId, locale));
                 String payFromPartyId = (String) rowMap.get("partyId");
 
-                Map<String,String> personInfoMap = null;
+                Map<String, String> personInfoMap = null;
 
-                Map<String,String> personAddressInfoMap = null;
+                Map<String, String> personAddressInfoMap = null;
 
                 //说明这笔订单我是卖家,查买家头像信息
-                if(payToPartyId.equals(partyId)){
-                    personInfoMap =   PersonManagerQueryServices.queryPersonBaseInfo(delegator, payFromPartyId);
+                if (payToPartyId.equals(partyId)) {
+                    personInfoMap = PersonManagerQueryServices.queryPersonBaseInfo(delegator, payFromPartyId);
                     personAddressInfoMap = PersonManagerQueryServices.queryPersonAddressInfo(delegator, payFromPartyId);
                 }
                 //说明这笔单我是买家,查卖家头像信息
-                if(!payToPartyId.equals(partyId)){
+                if (!payToPartyId.equals(partyId)) {
                     personInfoMap = PersonManagerQueryServices.queryPersonBaseInfo(delegator, payToPartyId);
                     personAddressInfoMap = PersonManagerQueryServices.queryPersonAddressInfo(delegator, payToPartyId);
                 }
-                rowMap.put("userPartyId",partyId);
-                rowMap.put("personInfoMap",personInfoMap);
-                rowMap.put("personAddressInfoMap",personAddressInfoMap);
+                rowMap.put("userPartyId", partyId);
+                rowMap.put("personInfoMap", personInfoMap);
+                rowMap.put("personAddressInfoMap", personAddressInfoMap);
 
                 returnList.add(rowMap);
             }
         }
 
 
-
-        resultMap.put("orderList",returnList);
+        resultMap.put("orderList", returnList);
 
         return resultMap;
     }

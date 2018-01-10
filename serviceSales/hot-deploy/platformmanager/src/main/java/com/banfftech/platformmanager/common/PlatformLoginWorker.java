6bd71ba0a7eae6dddc6bd521dc039dbeb1fcdbb8
s784838898 <784838898@qq.com>
2018-01-10 14:39:33 +0800

    Fixed Exception

--- serviceSales/hot-deploy/platformmanager/src/main/java/com/banfftech/platformmanager/common/PlatformLoginWorker.java
+++ serviceSales/hot-deploy/platformmanager/src/main/java/com/banfftech/platformmanager/common/PlatformLoginWorker.java
@@ -189,8 +189,12 @@ public class PlatformLoginWorker {
             //增加PartyId的返回
                 request.setAttribute("partyId",userLogin.get("partyId"));
             //增加人的名称返回
-            GenericValue nowPerson = delegator.findOne("Person", UtilMisc.toMap("partyId", userLogin.get("partyId")), false);
-            request.setAttribute("nowPersonName",nowPerson.get("firstName"));
+            try {
+                GenericValue nowPerson = delegator.findOne("Person", UtilMisc.toMap("partyId", userLogin.get("partyId")), false);
+                request.setAttribute("nowPersonName", nowPerson.get("firstName"));
+            }catch (GenericEntityException e) {
+
+            }
         } else {
             Debug.logWarning("*Could not find userLogin for token: " + token, module);
             Debug.logWarning("*Claims User Is : " + claims.get("user"), module);

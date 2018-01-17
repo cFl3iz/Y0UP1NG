46a146ce004afc17b79a266ec13357a6f5f0ee72
s784838898 <784838898@qq.com>
2018-01-17 14:38:30 +0800

    added distributingLeaflets

--- serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerQueryServices.java
+++ serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerQueryServices.java
@@ -481,6 +481,11 @@ public class PersonManagerQueryServices {
                 null, null, false).get(0);
         Map<String, Object> resourceDetail = product.getAllFields();
 
+        List<GenericValue> distributingLeafletsList =  EntityQuery.use(delegator).from(
+                "DistributingLeaflets").where("sellerPartyId",userLogin.get("partyId"),"buyerPartyId",realPartyId,"productId",product).queryList();
+        if(null != distributingLeafletsList && distributingLeafletsList.size()>0){
+            resultMap.put("distributingLeaflets",distributingLeafletsList.get(distributingLeafletsList.size()-1));
+        }
 
         resultMap.put("partyRelation",relationStr);
         resultMap.put("resourceDetail",resourceDetail);

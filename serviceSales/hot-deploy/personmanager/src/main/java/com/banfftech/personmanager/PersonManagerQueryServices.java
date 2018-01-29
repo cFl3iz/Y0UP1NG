52ffbbb145f7edbc68a023784122d324a9ac0b6f
s784838898 <784838898@qq.com>
2018-01-29 15:56:36 +0800

    默认zh

--- serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerQueryServices.java
+++ serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerQueryServices.java
@@ -455,7 +455,8 @@ public class PersonManagerQueryServices {
         //系统预设的FeatureType
         for(GenericValue gv : systemDefaultFeatureTypes){
             Map<String,Object> rowMap = gv.getAllFields();
-            rowMap.put("description",UtilProperties.getMessage("productEntityLabels.xml",(String)rowMap.get("description"), locale));
+
+            rowMap.put("description",UtilProperties.getMessage("productEntityLabels.xml",(String)rowMap.get("description"), new Locale("zh")));
             productFeatureList.add(rowMap);
         }
 

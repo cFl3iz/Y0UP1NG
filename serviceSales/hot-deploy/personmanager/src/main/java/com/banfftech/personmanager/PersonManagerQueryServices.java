2f5296e87ce8270b1322c8811f072c27baff5e50
s784838898 <784838898@qq.com>
2018-02-26 14:48:50 +0800

    Fixed bug

--- serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerQueryServices.java
+++ serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerQueryServices.java
@@ -29,12 +29,22 @@ import org.apache.ofbiz.entity.condition.EntityCondition;
 import org.apache.ofbiz.entity.util.EntityFindOptions;
 import org.apache.ofbiz.entity.condition.EntityConditionList;
 import org.apache.ofbiz.entity.condition.EntityOperator;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.eclipse.birt.chart.extension.datafeed.GanttEntry;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.awt.geom.GeneralPath;
+import java.io.UnsupportedEncodingException;
+import java.security.*;
+import java.security.spec.InvalidParameterSpecException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -42,7 +52,7 @@ import java.util.*;
 
 import main.java.com.banfftech.platformmanager.util.GZIP;
 import sun.net.www.content.text.Generic;
-import org.apache.commons.codec.binary.Base64;
+import org.codehaus.xfire.util.Base64;
 
 import static main.java.com.banfftech.platformmanager.util.HttpHelper.sendGet;
 
@@ -114,7 +124,7 @@ public class PersonManagerQueryServices {
      * @throws GenericServiceException
      * @throws ParseException
      */
-    public static Map<String, Object> getTelFromEncryptedData(DispatchContext dctx, Map<String, Object> context) throws GenericEntityException, GenericServiceException, Exception {
+    public Map<String, Object> getTelFromEncryptedData(DispatchContext dctx, Map<String, Object> context) throws GenericEntityException, GenericServiceException, Exception {
 
         //Service Head
         LocalDispatcher dispatcher = dctx.getDispatcher();
@@ -146,16 +156,52 @@ public class PersonManagerQueryServices {
 
       // UtilTools.decrypt(Base64.decodeBase64(session_key),Base64.decodeBase64(iv),Base64.decodeBase64(encryptedData));
 
-        byte[] resultByte  =  AesCbcUtil.decrypt(Base64.decodeBase64(encryptedData),Base64.decodeBase64(session_key),Base64.decodeBase64(iv));
-        if(null != resultByte && resultByte.length > 0){
-            String userInfo = new String(resultByte, "UTF-8");
-            System.out.println("userInfo = " + userInfo);
-        }else{
-
-        }
+        JSONObject json = getUserInfo(encryptedData,session_key,iv);
+        System.out.println("JSON DATA - > " +json);
         resultMap.put("tel","15000035538");
 
         return resultMap;
+    }
+
+    /**
+     * 解密用户敏感数据获取用户信息
+     * @param sessionKey 数据进行加密签名的密钥
+     * @param encryptedData 包括敏感数据在内的完整用户信息的加密数据
+     * @param iv 加密算法的初始向量
+     */
+    public JSONObject getUserInfo(String encryptedData,String sessionKey,String iv) throws UnsupportedEncodingException, InvalidAlgorithmParameterException, InvalidKeyException, InvalidParameterSpecException, BadPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, NoSuchPaddingException {
+        // 被加密的数据
+        byte[] dataByte = Base64.decode(encryptedData);
+        // 加密秘钥
+        byte[] keyByte = Base64.decode(sessionKey);
+        // 偏移量
+        byte[] ivByte = Base64.decode(iv);
+        try {
+            // 如果密钥不足16位，那么就补足.  这个if 中的内容很重要
+            int base = 16;
+            if (keyByte.length % base != 0) {
+                int groups = keyByte.length / base + (keyByte.length % base != 0 ? 1 : 0);
+                byte[] temp = new byte[groups * base];
+                Arrays.fill(temp, (byte) 0);
+                System.arraycopy(keyByte, 0, temp, 0, keyByte.length);
+                keyByte = temp;
+            }
+            // 初始化
+            Security.addProvider(new BouncyCastleProvider());
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding","BC");
+            SecretKeySpec spec = new SecretKeySpec(keyByte, "AES");
+            AlgorithmParameters parameters = AlgorithmParameters.getInstance("AES");
+            parameters.init(new IvParameterSpec(ivByte));
+            cipher.init(Cipher.DECRYPT_MODE, spec, parameters);// 初始化
+            byte[] resultByte = cipher.doFinal(dataByte);
+            if (null != resultByte && resultByte.length > 0) {
+                String result = new String(resultByte, "UTF-8");
+                return JSONObject.fromObject(result);
+            }
+        }catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 

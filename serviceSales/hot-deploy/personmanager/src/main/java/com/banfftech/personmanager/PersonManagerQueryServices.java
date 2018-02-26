280ca71c38744e58c75e71da22210c45a1bbcd54
s784838898 <784838898@qq.com>
2018-02-26 19:14:56 +0800

    更改GIT请求

--- serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerQueryServices.java
+++ serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerQueryServices.java
@@ -162,7 +162,7 @@ public class PersonManagerQueryServices {
 //        byte[] keyByte = Base64Util.decode(session_key);
 //        // 偏移量
 //        byte[] ivByte = Base64Util.decode(iv);
-
+        //TODO FIX ME  偏移量错误
        JSONObject json = getUserInfo("kxaFIw2PNrA/0Nb0r970l8s4/zgC6d+QFjZrRopIHy4clCdpCHUmxSj1dqFKPBa3odyTs80Pe1mr/v9F8+rSqsUYpekIVYjowdYQQPY8u2a8Gne811P0+nu572sq1jdyquIVT77VGB1BQF7Y22ppz9cjg6Zdz7rFvAGIG+6QQWN4QflH6byMaDNXDOi+YbRVM2RIhrBp7Bexya70ywkzog==",session_key,"EqKU4Vx70Y6FeunD6iZj+g==");
         //decrypt(keyByte,ivByte,dataByte);
 

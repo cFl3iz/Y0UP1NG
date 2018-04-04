7d42db38c5dd2b72aba66bf58a3da7c51df25372
s784838898 <784838898@qq.com>
2018-04-04 14:01:55 +0800

    Fixed bug

--- serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerServices.java
+++ serviceSales/hot-deploy/personmanager/src/main/java/com/banfftech/personmanager/PersonManagerServices.java
@@ -2229,23 +2229,23 @@ public class PersonManagerServices {
         GenericValue orderHeader = EntityQuery.use(delegator).from("OrderHeader").where("orderId", orderId).queryFirst();
 
 
-//        Map<String, Object> createOrderPaymentPreferenceMap = new HashMap<String, Object>();
-//        createOrderPaymentPreferenceMap.put("userLogin", admin);
-//        createOrderPaymentPreferenceMap.put("orderId", orderId);
-//        createOrderPaymentPreferenceMap.put("createdByUserLogin", userLogin.get("userLoginId"));
-//        createOrderPaymentPreferenceMap.put("maxAmount", orderHeader.get("grandTotal"));
-//        createOrderPaymentPreferenceMap.put("overflowFlag", "N");
-//        createOrderPaymentPreferenceMap.put("paymentMethodTypeId", "EXT_WXPAY");
-//        createOrderPaymentPreferenceMap.put("paymentMethodTypeId","EXT_COD");
-//        createOrderPaymentPreferenceMap.put("presentFlag", "N");
-//        createOrderPaymentPreferenceMap.put("statusId", "PAYMENT_RECEIVED");
-//        createOrderPaymentPreferenceMap.put("swipedFlag", "N");
+        Map<String, Object> createOrderPaymentPreferenceMap = new HashMap<String, Object>();
+        createOrderPaymentPreferenceMap.put("userLogin", admin);
+        createOrderPaymentPreferenceMap.put("orderId", orderId);
+        createOrderPaymentPreferenceMap.put("createdByUserLogin", userLogin.get("userLoginId"));
+        createOrderPaymentPreferenceMap.put("maxAmount", orderHeader.get("grandTotal"));
+        createOrderPaymentPreferenceMap.put("overflowFlag", "N");
+        createOrderPaymentPreferenceMap.put("paymentMethodTypeId", "EXT_WXPAY");
+        createOrderPaymentPreferenceMap.put("paymentMethodTypeId","EXT_COD");
+        createOrderPaymentPreferenceMap.put("presentFlag", "N");
+        createOrderPaymentPreferenceMap.put("statusId", "PAYMENT_RECEIVED");
+        createOrderPaymentPreferenceMap.put("swipedFlag", "N");
 
-//        Map<String, Object> createOrderPaymentPreferenceOutMap =
-//                dispatcher.runSync("createOrderPaymentPreference", createOrderPaymentPreferenceMap);
-//        if (!ServiceUtil.isSuccess(createOrderPaymentPreferenceOutMap)) {
-//            return createOrderPaymentPreferenceOutMap;
-//        }
+        Map<String, Object> createOrderPaymentPreferenceOutMap =
+                dispatcher.runSync("createOrderPaymentPreference", createOrderPaymentPreferenceMap);
+        if (!ServiceUtil.isSuccess(createOrderPaymentPreferenceOutMap)) {
+            return createOrderPaymentPreferenceOutMap;
+        }
 
 //        //找到买家
         GenericValue orderCust = EntityQuery.use(delegator).from("OrderRole").where("orderId", orderId, "roleTypeId", "SHIP_TO_CUSTOMER").queryFirst();
@@ -2261,7 +2261,7 @@ public class PersonManagerServices {
 //
 
         //查找订单支付Id
-         GenericValue orderPaymentPrefAndPayment = EntityQuery.use(delegator).from("OrderPaymentPrefAndPayment").where("orderId", orderId).queryFirst();
+   //      GenericValue orderPaymentPrefAndPayment = EntityQuery.use(delegator).from("OrderPaymentPrefAndPayment").where("orderId", orderId).queryFirst();
 
         //这种情况下说明是先付钱,后发货的。
 
@@ -2286,7 +2286,7 @@ public class PersonManagerServices {
 //        } else {
 //
 //
-            String orderPaymentPreferenceId = (String) orderPaymentPrefAndPayment.get("orderPaymentPreferenceId");
+      //      String orderPaymentPreferenceId = (String) orderPaymentPrefAndPayment.get("orderPaymentPreferenceId");
 //
 //            GenericValue orderHeader = EntityQuery.use(delegator).from("OrderHeader").where("orderId", orderId).queryFirst();
 //

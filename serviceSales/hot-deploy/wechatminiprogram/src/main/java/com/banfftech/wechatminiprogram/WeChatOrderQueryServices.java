e940ed00a3c1f0785554d90f2564ddeec00c6b2b
s784838898 <784838898@qq.com>
2018-04-24 11:33:42 +0800

    fixed bug

--- serviceSales/hot-deploy/wechatminiprogram/src/main/java/com/banfftech/wechatminiprogram/WeChatOrderQueryServices.java
+++ serviceSales/hot-deploy/wechatminiprogram/src/main/java/com/banfftech/wechatminiprogram/WeChatOrderQueryServices.java
@@ -268,8 +268,7 @@ public class WeChatOrderQueryServices {
             String innerDesc = gv.getString("description");
             switch (innerAttr) {
                 case "COLOR": {
-                    GenericValue isSelect = EntityQuery.use(delegator).from("ProductFeatureAndAppl")
-                            .where("productId", product, "productFeatureTypeId", "COLOR", "description",innerDesc).queryFirst();
+                    GenericValue isSelect = EntityQuery.use(delegator).from("ProductFeatureAndAppl").where("productId", product, "productFeatureTypeId", "COLOR", "description",innerDesc).queryFirst();
                     if(isSelect!=null){
                         innerMap.put("COLOR_DESC",innerDesc+"_SELECT");
                     }else{
@@ -278,8 +277,7 @@ public class WeChatOrderQueryServices {
                     break;
                 }
                 case "SIZE": {
-                    GenericValue isSelect = EntityQuery.use(delegator).from("ProductFeatureAndAppl")
-                            .where("productId", product, "productFeatureTypeId", "SIZE", "description",innerDesc).queryFirst();
+                    GenericValue isSelect = EntityQuery.use(delegator).from("ProductFeatureAndAppl").where("productId", product, "productFeatureTypeId", "SIZE", "description",innerDesc).queryFirst();
                     if(isSelect!=null){
                         innerMap.put("SIZE", innerDesc+"_SELECT");
                     }else{

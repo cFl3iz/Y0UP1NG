b58a9846403a2bfce34142e8027028fd223fd943
s784838898 <784838898@qq.com>
2018-04-25 18:54:35 +0800

    Fixed bug  added log

--- serviceSales/hot-deploy/wechatminiprogram/src/main/java/com/banfftech/wechatminiprogram/WeChatOrderQueryServices.java
+++ serviceSales/hot-deploy/wechatminiprogram/src/main/java/com/banfftech/wechatminiprogram/WeChatOrderQueryServices.java
@@ -157,9 +157,11 @@ public class WeChatOrderQueryServices {
 
                 rowMap.put("refreCount",refreCount+"");
 
-                EntityCondition findConditions = EntityCondition
-                        .makeCondition("productId", EntityOperator.LIKE, productId.substring(0,productId.indexOf("-")));
+                Debug.logInfo("ProductSales="+productId.substring(0, productId.indexOf("-")),module);
 
+                EntityCondition findConditions = EntityCondition.makeCondition("productId", EntityOperator.LIKE, "%"+productId.substring(0, productId.indexOf("-")) + "%");
+
+                
                 EntityCondition findConditions2 = EntityCondition.makeCondition("roleTypeId", EntityOperator.EQUALS, "SALES_REP");
                 EntityCondition findConditions3 = EntityCondition.makeCondition("partyId", EntityOperator.EQUALS,partyId);
                 EntityCondition genericCondition = EntityCondition.makeCondition(findConditions, EntityOperator.AND, findConditions2);

5206247512b60eb27890197e86be5226da731f15
S <784838898@qq.com>
2018-11-22 10:23:53 +0800

    减少viewSize

--- serviceSales/hot-deploy/wechatminiprogram/src/main/java/com/banfftech/wechatminiprogram/WeChatOrderQueryServices.java
+++ serviceSales/hot-deploy/wechatminiprogram/src/main/java/com/banfftech/wechatminiprogram/WeChatOrderQueryServices.java
@@ -2325,7 +2325,7 @@ public class WeChatOrderQueryServices {
             viewIndex = Integer.parseInt(viewIndexStr);
         }
 
-        int viewSize = 50000;
+        int viewSize = 10;
         int lowIndex = 0;
         int highIndex = 0;
         Long resourceCount;
